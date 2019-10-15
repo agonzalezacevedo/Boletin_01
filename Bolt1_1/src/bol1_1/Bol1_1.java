4861ca67bec7350772589c545a1d6761e39cce99
Alexandre Johannes González Acevedo <agonzalezacevedo@nautilus10.danielcastelao.org>
2019-10-15 13:19:56 +0200

    Boletin1_1

---
+++ Bolt1_1/src/bol1_1/Bol1_1.java
@@ -0,0 +1,12 @@
+package bol1_1;
+
+public class Bol1_1 {
+
+    public static void main(String[] args){
+        int base = 4, altura = 3, area;
+        area = (base*altura)/2;
+        System.out.println("El area de un triangulo de base=4 y altura=3 es igual a "+area);
+        
+    }
+    
+}
